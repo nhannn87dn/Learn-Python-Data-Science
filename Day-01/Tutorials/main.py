bf9606e03703b571fa06e18b03d9e424c086c85e
nhannn87dn <nhannn87dn@gmail.com>
2025-11-10 20:45:03 +0700

    day1

---
+++ Day-01/Tutorials/main.py
@@ -0,0 +1,42 @@
+print("Hello world !")
+print(123)
+print(True)
+# Làm sao có thể in ra nhiều dòng bằng 1 lần print thôi ?
+#print(This will cause an error) # Sai cú pháp
+
+
+print("""Đây là một chuỗi
+nhiều dòng.
+Dòng thứ hai.
+jhsjdsh
+djsjdhsj
+jdhsjdshd
+Dòng thứ ba.""")
+
+# Hãy nói: 'Xin chào !'
+print('Hãy nói: "Xin chào !"')
+print("Hãy nói: 'Xin chào !'")
+
+# I'm a student
+print("I'm a student")
+print('I\'m a student')  # Sử dụng ký tự escape \
+
+# option sep: để nối các value lại với nhau mặc định  " "
+# end: tuỳ chọn theo kí tự vào giá trị cuối
+print('value 1', 123, 'value 2', sep="\n")
+
+## Phần 2: Comment - Ghi chú
+# Sử dụng dấu thăng # đặt trước dòng lệnh
+# Khi mình ko muốn 1 dòng code nào đó chạy, 
+# và cũng ko muốn xoá ra khỏi file
+
+# print('abc') single comment (Comment 1 dòng)
+
+'''print('an')
+print(123)
+print(True)'''
+
+# CTRL + /
+# print('an')
+# print(123)
+# print(True)
