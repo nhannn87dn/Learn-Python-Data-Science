3408d02ce90599a2a3d3c8cea0f7b80385ca5446
nhannn87dn <nhannn87dn@gmail.com>
2025-11-12 16:17:03 +0700

    day4

---
+++ Day-04/tutorials/loop.py
@@ -0,0 +1,17 @@
+# for x in "banana":
+#     print(x)
+
+
+# Vòng lặp while
+
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
+    
+    
+for x in range(1,5):
+    if x == 2:
+        #break
+        continue
+    print(x)
---
+++ Day-04/tutorials/range.py
@@ -0,0 +1,16 @@
+# x = range(9, 0, -1)
+# for i in x:
+#     print(i)
+    
+# In ra một dãy số chẵn từ 2-10
+# n = range(2, 11, 2)
+# for i in n:
+#     print(i)
+    
+# Cho một dãy số từ 1 - 10.
+# Hãy in những số chẵn thôi.
+
+# m = range(1,11)
+# for i in m:
+#     if i % 2 == 0:
+#         print(i)
